4700b29b9be691b1f05608984cbc597d1e7fc5c2
CephasKwabieC <cephadinho1@gmail.com>
2024-05-03 09:07:04 +0000

    prep 02 activity complete

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,13 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.Write("What is your last name? ");
-        string lastname = Console.ReadLine();
+    { 
+        Console.Write("what is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int gradePercentage = int.Parse(answer);
 
-        Console.Write("What is your first name? ");
-        string firstname = Console.ReadLine();
+        string letter = "";
 
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+        if (gradePercentage >= 90)
+        {
+            letter = "A"; 
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradePercentage < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed! Congratulations!");
+        }
+        else if (gradePercentage < 70)
+        {
+            Console.WriteLine("You failed! Better luck next time.");
+        }
+
     }
 }
